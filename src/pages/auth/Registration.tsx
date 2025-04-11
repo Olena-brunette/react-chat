31cd57f9bf4b82a90a96765d1d54823022e3a5b2
unknown <elena.astashkevych@gmail.com>
2025-04-11 22:32:27 +0300

    temporary fix

--- src/pages/auth/Registration.tsx
+++ src/pages/auth/Registration.tsx
@@ -21,32 +21,32 @@ export const RegistrationPage = () => {
   const context = useContext(ChatContext);
   const navigate = useNavigate();
 
-  const fields = [
-    {
-      name: 'login',
-      type: 'login',
-      placeholder: 'Login',
-      required: true,
-      errorText: 'Please enter a valid Login',
-      errorRegex: /^[a-zA-Z0-9]{6,}$/,
-    },
-    {
-      name: 'password',
-      type: 'password',
-      placeholder: 'Password',
-      required: true,
-      errorText: 'Please enter a valid password',
-      errorRegex: /^[a-zA-Z0-9]{6,}$/,
-    },
-    {
-      name: 'passwordConfirm',
-      type: 'password',
-      placeholder: 'Confirm Password',
-      required: true,
-      errorText: 'Passwords do not match',
-      errorRegex: /^[a-zA-Z0-9]{6,}$/,
-    },
-  ];
+  // const fields = [
+  //   // {
+  //   //   name: 'login',
+  //   //   type: 'login',
+  //   //   placeholder: 'Login',
+  //   //   required: true,
+  //   //   errorText: 'Please enter a valid Login',
+  //   //   errorRegex: /^[a-zA-Z0-9]{6,}$/,
+  //   // },
+  //   // {
+  //   //   name: 'password',
+  //   //   type: 'password',
+  //   //   placeholder: 'Password',
+  //   //   required: true,
+  //   //   errorText: 'Please enter a valid password',
+  //   //   errorRegex: /^[a-zA-Z0-9]{6,}$/,
+  //   // },
+  //   // {
+  //   //   name: 'passwordConfirm',
+  //   //   type: 'password',
+  //   //   placeholder: 'Confirm Password',
+  //   //   required: true,
+  //   //   errorText: 'Passwords do not match',
+  //   //   errorRegex: /^[a-zA-Z0-9]{6,}$/,
+  //   // },
+  // ];
 
   const defaultValues = {
     login: '',
@@ -59,32 +59,51 @@ export const RegistrationPage = () => {
   });
 
   const registration = async ({ login, password }: FormValues) => {
-    setLoading(true);
-    try {
-      const user = await registerUser({ login, password });
-      if (user) {
-        context?.setUser(user);
-        setAccessToken(user.accessToken);
-        AuthService.saveTokens(user.accessToken, user.refreshToken);
-        navigate('/chat');
-      }
-    } catch (e) {
-      if (e instanceof Error) {
-        setError(e.message);
-      } else {
-        setError('An unknown error occurred');
-      }
-    } finally {
-      setLoading(false);
+    // temporary solution for registration
+  const  res = await fetch('https://yabbering-anna-diana-testolena-b2a23eb2.koyeb.app/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "email": "ggg@test.com",
+        "username": "testttt",
+        "password": "admin123strinG"
+      }),
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      console.error('Registration failed:', data.message);
+      return;
     }
+    console.log('Registration successful:', data);
+
+    // setLoading(true);
+    // try {
+    //   const user = await registerUser({ login, password });
+    //   if (user) {
+    //     context?.setUser(user);
+    //     setAccessToken(user.accessToken);
+    //     AuthService.saveTokens(user.accessToken, user.refreshToken);
+    //     navigate('/chat');
+    //   }
+    // } catch (e) {
+    //   if (e instanceof Error) {
+    //     setError(e.message);
+    //   } else {
+    //     setError('An unknown error occurred');
+    //   }
+    // } finally {
+    //   setLoading(false);
+    // }
   };
 
   return (
     <div className="registration-wrapper">
       <div className={`${loading ? 'loader' : ''}`}></div>
-      <FormProvider {...methods}>
-        <Form fields={fields} title="Registration" />
-      </FormProvider>
+      {/* <FormProvider {...methods}> */}
+        {/* <Form fields={fields} title="Registration" /> */}
+      {/* </FormProvider> */}
       <button className="auth-btn" onClick={methods.handleSubmit(registration)}>
         Sign up
       </button>
